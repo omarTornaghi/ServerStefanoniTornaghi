7e7064b740a06b864d9cb7afae21d7322cefa6e2
omarTornaghi <omar.tor2011@gmail.com>
2021-06-23 11:44:12 +0200

    Deposito

--- src/Data.java
+++ src/Data.java
@@ -1,6 +1,9 @@
 public class Data {
     private String valore;
 
+    public Data(){}
+    public Data(String valore){ this.valore = valore;}
+
     public void setValore(String v){ valore = v;}
     public String getValore(){return valore; }
 }
